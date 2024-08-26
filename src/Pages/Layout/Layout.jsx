5faa522d71c7dd9eba95aa3c7e4d739ae77c9466
Amit Banjare <amitbanjare79@gmail.com>
2024-08-26 10:56:21 +0530

    create section 8

--- src/Pages/Layout/Layout.jsx
+++ src/Pages/Layout/Layout.jsx
@@ -7,6 +7,7 @@ import Section2 from "../Section/Section2/Section2";
 import Section3 from "../Section/Section3/Section3";
 import Section4 from "../Section/Section4.jsx/Section4";
 import CustomCard from "../Section/Section7/Section7";
+import Section8 from "../Section/Section8/Section8";
 
 const Layout = () => {
   return (
@@ -14,10 +15,11 @@ const Layout = () => {
       <Header />
       <main style={{ padding: "20px" }}>
         <Section1 />
-        <Section2 />
-        <Section3 />
+        {/* <Section2 /> */}
+        {/* <Section3 /> */}
          <Section4 />
-         <CustomCard />
+         {/* <CustomCard /> */}
+         <Section8 />
       </main>
       <Footer />
     </>
